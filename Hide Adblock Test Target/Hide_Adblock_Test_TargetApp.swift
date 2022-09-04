d704cedfb9f0835be598fbf1e8a6e781cc68a55b
WhyNotWhyYes <artemiy.zuzin@gmail.com>
2022-09-04 15:34:58 +0300

    Initial Commit

---
+++ Hide Adblock Test Target/Hide_Adblock_Test_TargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hide_Adblock_Test_TargetApp.swift
+//  Hide Adblock Test Target
+//
+//  Created by Artemiy Zuzin on 04.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hide_Adblock_Test_TargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
